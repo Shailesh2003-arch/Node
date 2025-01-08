7fd3d3d71cbcb41ab497f8b809826971c2f68479
Shailesh2003-arch <117181669+Shailesh2003-arch@users.noreply.github.com>
2025-01-08 21:06:50 +0530

    done-with-request-respone

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 // creating the server using the inbuilt module http.
 
 const http = require("http");
+const fs = require("fs");
+
+const html = fs.readFileSync("./template/index.html", "utf-8");
 
 const respone = http.createServer((req, res) => {
   //   console.log("A new request hit the server");
-  //   res.end(`Hello from the server`);
+  res.end(html);
   console.log(`another req`);
   //   console.log(req);
   //   console.log(res);
