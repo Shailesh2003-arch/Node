0f115ba865c9f4d7932a3e24a4fec3b6286cc57d
Shailesh2003-arch <117181669+Shailesh2003-arch@users.noreply.github.com>
2025-01-08 20:48:28 +0530

    update

--- server.js
+++ server.js
@@ -3,7 +3,11 @@
 const http = require("http");
 
 const respone = http.createServer((req, res) => {
-  console.log("A new request hit the server");
+  //   console.log("A new request hit the server");
+  //   res.end(`Hello from the server`);
+  console.log(`another req`);
+  //   console.log(req);
+  //   console.log(res);
 });
 
 respone.listen(8000, "127.0.0.1", () => {
