d3fda8887aa23c222627406eb3587371c0aed0ad
Shailesh2003-arch <117181669+Shailesh2003-arch@users.noreply.github.com>
2025-01-12 22:13:35 +0530

    done-with-routing

--- server.js
+++ server.js
@@ -7,8 +7,19 @@ const html = fs.readFileSync("./template/index.html", "utf-8");
 
 const respone = http.createServer((req, res) => {
   //   console.log("A new request hit the server");
-  res.end(html);
-  console.log(`another req`);
+  // res.end(html);
+  let path = req.url;
+  console.log(path);
+  if (path === "/" || path.toLowerCase() === "/home") {
+    res.end(`You are at home page`);
+  } else if (path.toLocaleLowerCase() === "/about") {
+    res.end(`You are in about page`);
+  } else if (path.toLocaleLowerCase() === "/contact") {
+    res.end(`You are in contact page`);
+  } else {
+    res.end(`Error 404:Page not found`);
+  }
+
   //   console.log(req);
   //   console.log(res);
 });
