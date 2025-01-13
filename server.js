eb06b0a36579e478d7028ecfe7455908cb62fa7d
Shailesh2003-arch <117181669+Shailesh2003-arch@users.noreply.github.com>
2025-01-13 11:07:39 +0530

    done-with-setting-headers

--- server.js
+++ server.js
@@ -11,14 +11,35 @@ const respone = http.createServer((req, res) => {
   let path = req.url;
   console.log(path);
   if (path === "/" || path.toLowerCase() === "/home") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+      "my-header": "hello world",
+    });
+
     res.end(html.replace("{{%Content%}}", "You are in home page"));
   } else if (path.toLocaleLowerCase() === "/products") {
+    res.writeHead(200);
     res.end(html.replace("{{%Content%}}", "You are in products page"));
   } else if (path.toLocaleLowerCase() === "/about") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+      "my-header": "hello world",
+    });
+
     res.end(html.replace("{{%Content%}}", "You are in about page"));
   } else if (path.toLocaleLowerCase() === "/contact") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+      "my-header": "hello world",
+    });
+
     res.end(html.replace("{{%Content%}}", "You are in contact page"));
   } else {
+    res.writeHead(404, {
+      "Content-Type": "text/html",
+      "my-header": "sorry the page you're finding doesn't exist",
+    });
+
     res.end(html.replace("{{%Content%}}", "Error: 404 page not found"));
   }
 });
